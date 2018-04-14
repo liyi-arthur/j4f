c522f369e9e21baee3b507559ea90c077905e233
liyi <liyi_whu@163.com>
2018-04-15 01:33:20 +0800

    update final version

--- py/readprocmem.py
+++ py/readprocmem.py
@@ -1,6 +1,7 @@
 
 import getopt
 import os, sys
+import re
 
 def showHelp():
     print("Read memory info of a process in Linux")
@@ -9,8 +10,37 @@ def showHelp():
     print("   -a  Memory address")
     print("   -s  Size of memory to show")
 
+def printMemInHex(chunk, size):
+    i = 0
+    chunk_str = chunk[:]
+    offset = 0
+    for b in chunk:
+        i = i + 1
+        offset = offset + 1
+        print(hex(ord(b))[2:].zfill(2) + " "),
+        if i < 16: continue
+        print("   " * (16 - i)),
+        print("     : "),
+        print(chunk_str[(offset-i):offset])
+        i = 0
+    #pass
+
 def readProcMem(p, a, s):
-    pass
+    ppath = "/proc/" + str(p) + "/"
+    maps_file = open(ppath + "maps", 'r')
+    mem_file = open(ppath + "mem", 'r', 0)
+    for line in maps_file.readlines():  # for each mapped region
+        m = re.match(r'([0-9A-Fa-f]+)-([0-9A-Fa-f]+) ([-r])', line)
+        if m.group(3) == 'r':  # if this is a readable region
+            start = int(m.group(1), 16)
+            end = int(m.group(2), 16)
+            
+            if a >= start and a <= end:
+                mem_file.seek(a)  # seek to region start
+                chunk = mem_file.read(min(s, (end - a)))
+                printMemInHex(chunk, min(s, (end - a)))
+    maps_file.close()
+    mem_file.close()
 
 def getAddrFromInput(v):
     if len(v) <= 2: return int(v)
