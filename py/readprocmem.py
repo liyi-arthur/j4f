9a4d6c56b0b99288ba10dcb3570ce94b40490ddb
liyi <liyi_whu@163.com>
2018-04-14 23:35:12 +0800

    add read process memory function

---
+++ py/readprocmem.py
@@ -0,0 +1,28 @@
+
+import getopt
+import os, sys
+
+def showHelp():
+    print("Read memory info of a process in Linux")
+    print("Parameters:")
+    print("   -p  Process id")
+    print("   -a  Memory address")
+    print("   -s  Size of memory to show")
+
+def getAddrFromInput(v):
+    if len(v) <= 2: return int(v)
+
+    if v[:2] == "0x" or v[:2] == "0X": return int(v, 16)
+    else:                              return int(v)
+
+if __name__ == '__main__':
+    opts, args = getopt.getopt(sys.argv[1:], 'hp:a:s:')
+
+    pid, addr, size = 0, 0, 0
+    for k, v in opts:
+        if k == "-h": showHelp()
+        if k == "-p": pid = int(v)
+        if k == "-a": addr = getAddrFromInput(v)
+        if k == "-s": size = int(v)
+        
+    print(pid, addr, size)
